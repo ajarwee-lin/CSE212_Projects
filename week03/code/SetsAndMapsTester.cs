5766c919d2448825ec2f0cdde5e961899188c14d
A. Lincoln Jarwee <122073281+ajarwee-lin@users.noreply.github.com>
2024-06-27 21:33:57 -0600

    commit new changes to my Git

--- week03/code/SetsAndMapsTester.cs
+++ week03/code/SetsAndMapsTester.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 
 public static class SetsAndMapsTester {
@@ -5,60 +10,43 @@ public static class SetsAndMapsTester {
         // Problem 1: Find Pairs with Sets
         Console.WriteLine("\n=========== Finding Pairs TESTS ===========");
         DisplayPairs(new[] { "am", "at", "ma", "if", "fi" });
-        // ma & am
-        // fi & if
         Console.WriteLine("---------");
         DisplayPairs(new[] { "ab", "bc", "cd", "de", "ba" });
-        // ba & ab
         Console.WriteLine("---------");
         DisplayPairs(new[] { "ab", "ba", "ac", "ad", "da", "ca" });
-        // ba & ab
-        // da & ad
-        // ca & ac
         Console.WriteLine("---------");
-        DisplayPairs(new[] { "ab", "ac" }); // No pairs displayed
+        DisplayPairs(new[] { "ab", "ac" });
         Console.WriteLine("---------");
         DisplayPairs(new[] { "ab", "aa", "ba" });
-        // ba & ab
         Console.WriteLine("---------");
         DisplayPairs(new[] { "23", "84", "49", "13", "32", "46", "91", "99", "94", "31", "57", "14" });
-        // 32 & 23
-        // 94 & 49
-        // 31 & 13
 
         // Problem 2: Degree Summary
-        // Sample Test Cases (may not be comprehensive) 
         Console.WriteLine("\n=========== Census TESTS ===========");
         Console.WriteLine(string.Join(", ", SummarizeDegrees("census.txt")));
-        // Results may be in a different order:
-        // <Dictionary>{[Bachelors, 5355], [HS-grad, 10501], [11th, 1175],
-        // [Masters, 1723], [9th, 514], [Some-college, 7291], [Assoc-acdm, 1067],
-        // [Assoc-voc, 1382], [7th-8th, 646], [Doctorate, 413], [Prof-school, 576],
-        // [5th-6th, 333], [10th, 933], [1st-4th, 168], [Preschool, 51], [12th, 433]}
 
         // Problem 3: Anagrams
-        // Sample Test Cases (may not be comprehensive) 
         Console.WriteLine("\n=========== Anagram TESTS ===========");
-        Console.WriteLine(IsAnagram("CAT", "ACT")); // true
-        Console.WriteLine(IsAnagram("DOG", "GOOD")); // false
-        Console.WriteLine(IsAnagram("AABBCCDD", "ABCD")); // false
-        Console.WriteLine(IsAnagram("ABCCD", "ABBCD")); // false
-        Console.WriteLine(IsAnagram("BC", "AD")); // false
-        Console.WriteLine(IsAnagram("Ab", "Ba")); // true
-        Console.WriteLine(IsAnagram("A Decimal Point", "Im a Dot in Place")); // true
-        Console.WriteLine(IsAnagram("tom marvolo riddle", "i am lord voldemort")); // true
-        Console.WriteLine(IsAnagram("Eleven plus Two", "Twelve Plus One")); // true
-        Console.WriteLine(IsAnagram("Eleven plus One", "Twelve Plus One")); // false
+        Console.WriteLine(IsAnagram("CAT", "ACT"));
+        Console.WriteLine(IsAnagram("DOG", "GOOD"));
+        Console.WriteLine(IsAnagram("AABBCCDD", "ABCD"));
+        Console.WriteLine(IsAnagram("ABCCD", "ABBCD"));
+        Console.WriteLine(IsAnagram("BC", "AD"));
+        Console.WriteLine(IsAnagram("Ab", "Ba"));
+        Console.WriteLine(IsAnagram("A Decimal Point", "Im a Dot in Place"));
+        Console.WriteLine(IsAnagram("tom marvolo riddle", "i am lord voldemort"));
+        Console.WriteLine(IsAnagram("Eleven plus Two", "Twelve Plus One"));
+        Console.WriteLine(IsAnagram("Eleven plus One", "Twelve Plus One"));
 
         // Problem 4: Maze
         Console.WriteLine("\n=========== Maze TESTS ===========");
-        Dictionary<ValueTuple<int, int>, bool[]> map = SetupMazeMap();
+        Dictionary<(int, int), bool[]> map = SetupMazeMap();
         var maze = new Maze(map);
-        maze.ShowStatus(); // Should be at (1,1)
-        maze.MoveUp(); // Error
-        maze.MoveLeft(); // Error
+        maze.ShowStatus();
+        maze.MoveUp();
+        maze.MoveLeft();
         maze.MoveRight();
-        maze.MoveRight(); // Error
+        maze.MoveRight();
         maze.MoveDown();
         maze.MoveDown();
         maze.MoveDown();
@@ -68,104 +56,65 @@ public static class SetsAndMapsTester {
         maze.MoveRight();
         maze.MoveDown();
         maze.MoveLeft();
-        maze.MoveDown(); // Error
+        maze.MoveDown();
         maze.MoveRight();
         maze.MoveDown();
         maze.MoveDown();
         maze.MoveRight();
-        maze.ShowStatus(); // Should be at (6,6)
+        maze.ShowStatus();
 
         // Problem 5: Earthquake
-        // Sample Test Cases (may not be comprehensive) 
         Console.WriteLine("\n=========== Earthquake TESTS ===========");
         EarthquakeDailySummary();
-
-        // Sample output from the function.  Number of earthquakes, places, and magnitudes will vary.
-        // 1km NE of Pahala, Hawaii - Mag 2.36
-        // 58km NW of Kandrian, Papua New Guinea - Mag 4.5
-        // 16km NNW of Truckee, California - Mag 0.7
-        // 9km S of Idyllwild, CA - Mag 0.25
-        // 14km SW of Searles Valley, CA - Mag 0.36
-        // 4km SW of Volcano, Hawaii - Mag 1.99
     }
 
-    /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for displaying all symmetric pairs of words.  
-    ///
-    /// For example, if <c>words</c> was: <c>[am, at, ma, if, fi]</c>, we would display:
-    /// <code>
-    /// am &amp; ma
-    /// if &amp; fi
-    /// </code>
-    /// The order of the display above does not matter. <c>at</c> would not 
-    /// be displayed because <c>ta</c> is not in the list of words.
-    ///
-    /// As a special case, if the letters are the same (example: 'aa') then
-    /// it would not match anything else (remember the assumption above
-    /// that there were no duplicates) and therefore should not be displayed.
-    /// </summary>
-    /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     private static void DisplayPairs(string[] words) {
-        // To display the pair correctly use something like:
-        // Console.WriteLine($"{word} & {pair}");
-        // Each pair of words should displayed on its own line.
+        var set = new HashSet<string>();
+        foreach (var word in words) {
+            var reversed = new string(word.Reverse().ToArray());
+            if (set.Contains(reversed)) {
+                Console.WriteLine($"{reversed} & {word}");
+            } else {
+                set.Add(word);
+            }
+        }
     }
 
-    /// <summary>
-    /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
-    /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
-    /// file.
-    /// </summary>
-    /// <param name="filename">The name of the file to read</param>
-    /// <returns>fixed array of divisors</returns>
-    /// #############
-    /// # Problem 2 #
-    /// #############
     private static Dictionary<string, int> SummarizeDegrees(string filename) {
         var degrees = new Dictionary<string, int>();
         foreach (var line in File.ReadLines(filename)) {
             var fields = line.Split(",");
-            // Todo Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3];
+            if (degrees.ContainsKey(degree)) {
+                degrees[degree]++;
+            } else {
+                degrees[degree] = 1;
+            }
         }
-
         return degrees;
     }
 
-    /// <summary>
-    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
-    /// new word.  A dictionary is used to solve the problem.
-    /// 
-    /// Examples:
-    /// is_anagram("CAT","ACT") would return true
-    /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
-    /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
-    /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
-    /// using the [] notation.
-    /// </summary>
-    /// #############
-    /// # Problem 3 #
-    /// #############
     private static bool IsAnagram(string word1, string word2) {
-        // Todo Problem 3 - ADD YOUR CODE HERE
-        return false;
+        var dict = new Dictionary<char, int>();
+        foreach (var ch in word1.ToLower().Replace(" ", "")) {
+            if (dict.ContainsKey(ch)) {
+                dict[ch]++;
+            } else {
+                dict[ch] = 1;
+            }
+        }
+        foreach (var ch in word2.ToLower().Replace(" ", "")) {
+            if (!dict.ContainsKey(ch) || dict[ch] == 0) {
+                return false;
+            } else {
+                dict[ch]--;
+            }
+        }
+        return dict.Values.All(x => x == 0);
     }
 
-    /// <summary>
-    /// Sets up the maze dictionary for problem 4
-    /// </summary>
-    private static Dictionary<ValueTuple<int, int>, bool[]> SetupMazeMap() {
-        Dictionary<ValueTuple<int, int>, bool[]> map = new() {
+    private static Dictionary<(int, int), bool[]> SetupMazeMap() {
+        Dictionary<(int, int), bool[]> map = new() {
             { (1, 1), new[] { false, true, false, true } },
             { (1, 2), new[] { false, true, true, false } },
             { (1, 3), new[] { false, false, false, false } },
@@ -206,20 +155,6 @@ public static class SetsAndMapsTester {
         return map;
     }
 
-    /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
-    /// United States Geological Service (USGS) consisting of earthquake data.
-    /// The data will include all earthquakes in the current day.
-    /// 
-    /// JSON data is organized into a dictionary. After reading the data using
-    /// the built-in HTTP client library, this function will print out a list of all
-    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
-    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
-    /// </summary>
     private static void EarthquakeDailySummary() {
         const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
         using var client = new HttpClient();
@@ -231,9 +166,34 @@ public static class SetsAndMapsTester {
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        // TODO:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to print out each place a earthquake has happened today and its magitude.
+        foreach (var feature in featureCollection.Features) {
+            Console.WriteLine($"{feature.Properties.Place} - Mag {feature.Properties.Mag}");
+        }
     }
 }
+
+public class FeatureCollection {
+    public List<Feature> Features { get; set; }
+}
+
+public class Feature {
+    public Properties Properties { get; set; }
+}
+
+public class Properties {
+    public string Place { get; set; }
+    public double Mag { get; set; }
+}
+
+public class Maze {
+    private Dictionary<(int, int), bool[]> _map;
+    private (int, int) _position;
+
+    public Maze(Dictionary<(int, int), bool[]> map) {
+        _map = map;
+        _position = (1, 1);
+    }
+
+    public void MoveUp() {
+        if (_map[_position][2]) {
+            _position = (_position
